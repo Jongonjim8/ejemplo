e34d27218aa350dec794d4fee9d59973f17cca57
Jonatan Jimenez Gonzalez <jonatan.jimenez@itsn.edu.mx>
2026-02-27 22:35:54 -0600

    Fix SOAP address

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const soap = require('soap');
+const fs = require('fs');
+
 const app = express();
 
-app.use(express.json());
-
+// REST
 app.get('/saludo', (req, res) => {
-    res.json({ mensaje: "Hola desde mi servicio REST en Render 🚀" });
+    res.json({ mensaje: "Hola desde REST 🚀" });
 });
 
+// SOAP service definition
+const service = {
+  SaludoService: {
+    SaludoPort: {
+      saludar: function(args) {
+        return { respuesta: "Hola " + args.nombre };
+      }
+    }
+  }
+};
+
+const xml = fs.readFileSync('service.wsdl', 'utf8');
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
+    soap.listen(app, '/wsdl', service, xml);
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
